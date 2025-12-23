0e0ec7a2c9cbecc23fecc965d42b078ac284f830
harshitha <harshithaav290419@gmail.com>
2025-12-23 18:07:58 +0530

    Simplify CD to deploy using only deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-set -e
+set -x
 
+echo "Logging into ECR"
 aws ecr get-login-password --region ap-south-1 | \
 docker login --username AWS --password-stdin 311153964827.dkr.ecr.ap-south-1.amazonaws.com
 
-docker stack deploy -c docker-stack.yml demo
+echo "Deploying docker stack"
+docker stack deploy \
+  --with-registry-auth \
+  --resolve-image always \
+  -c docker-stack.yml demo-backend
+
+echo "Deployment completed"
