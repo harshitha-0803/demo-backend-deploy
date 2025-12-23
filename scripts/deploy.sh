13132ec57f16f54864f43049236bc04910f1e62b
harshitha <harshithaav290419@gmail.com>
2025-12-23 14:39:42 +0530

    Add CodeDeploy appspec, scripts, and docker stack file

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+aws ecr get-login-password --region ap-south-1 | \
+docker login --username AWS --password-stdin 311153964827.dkr.ecr.ap-south-1.amazonaws.com
+
+docker stack deploy -c docker-stack.yml demo
