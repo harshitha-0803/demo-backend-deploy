d8d85333b01dd88d6b35cb2b42b7776c9efc04ea
harshitha <harshithaav290419@gmail.com>
2025-12-23 14:51:24 +0530

    Remove unwanted deployment files

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-aws ecr get-login-password --region ap-south-1 | \
-docker login --username AWS --password-stdin 311153964827.dkr.ecr.ap-south-1.amazonaws.com
-
-docker stack deploy -c docker-stack.yml demo
