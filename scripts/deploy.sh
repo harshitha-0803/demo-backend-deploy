ba3ccb47ab9bd43881869c5f149e0b14b6954bbe
harshitha <harshithaav290419@gmail.com>
2025-12-26 17:20:19 +0530

    Add deploy.sh under scripts directory

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -x
+
+echo "Logging into ECR"
+aws ecr get-login-password --region ap-south-1 \
+ | docker login --username AWS --password-stdin 311153964827.dkr.ecr.ap-south-1.amazonaws.com
+
+echo "Deploying docker stack"
+docker stack deploy \
+  --with-registry-auth \
+  --resolve-image always \
+  -c deployment-archive/docker-stack.yml \
+  demo-backend
+
+echo "Deployment completed"
