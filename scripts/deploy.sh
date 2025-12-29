1a7248db83152043f5ce1da7f9ced4e63a920a3c
harshitha <harshithaav290419@gmail.com>
2025-12-29 15:15:54 +0530

    Fix CodeDeploy paths and docker-stack location

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,17 @@ set -x
 
 echo "Logging into ECR"
 aws ecr get-login-password --region ap-south-1 \
- | docker login --username AWS --password-stdin 311153964827.dkr.ecr.ap-south-1.amazonaws.com
+| docker login --username AWS --password-stdin 311153964827.dkr.ecr.ap-south-1.amazonaws.com
+
+echo "Changing directory"
+cd /opt/demo-backend
+
+echo "Listing files"
+ls -la
 
 echo "Deploying docker stack"
 docker stack deploy \
   --with-registry-auth \
   --resolve-image always \
-  -c docker-stack.yml \
-  demo-backend
+  -c docker-stack.yml demo-backend
 
-echo "Deployment completed"
